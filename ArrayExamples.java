31c3227a1675cc94cb6a33ca8aec28155045ad52
pranavprabu22 <48165881+pranavprabu22@users.noreply.github.com>
2023-01-30 19:15:42 -0800

    yes
    yes

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -3,13 +3,10 @@
 public class ArrayExamples {
 
   // Changes the input array to be in reversed order
-  static int[] reverseInPlace(int[] arr) {
-    for(int i = 0; i < arr.length/2; i += 1) {
-      int temp = arr[i];
+  static void reverseInPlace(int[] arr) {
+    for(int i = 0; i < arr.length; i += 1) {
       arr[i] = arr[arr.length - i - 1];
-      arr[arr.length - i - 1] = temp;
     }
-    return arr;
   }
 
   // Returns a *new* array with all the elements of the input array in reversed
--- ArrayTests.java
+++ ArrayTests.java
@@ -2,18 +2,12 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
-	@Test 
-	public void testReverseInPlace() {
-    int[] input1 = { 3 };
-    ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
-	}
 
   @Test
-  public void testReverseInPlace2() {
-    int[] input1 = {1,2};
+  public void testReverseInPlace() {
+    int[] input1 = {1, 2, 3, 2, 1};
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{2,1}, input1);
+    assertArrayEquals(new int[]{1, 2, 3, 2, 1}, input1);
 	}
 
   @Test
